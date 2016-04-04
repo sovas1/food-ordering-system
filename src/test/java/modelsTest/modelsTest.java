981884a8e7b2692fb1a9e3d50ef07a5712440a9c
sovas <mariuszsowa11@gmail.com>
2016-04-04 12:45:57 +0200

    come tests

--- src/test/java/modelsTest/modelsTest.java
+++ src/test/java/modelsTest/modelsTest.java
@@ -1,4 +1,32 @@
 package modelsTest;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.sovas.models.Lunch;
+import org.sovas.models.lunchParts.Desert;
+import org.sovas.models.lunchParts.MainCourse;
+
 public class modelsTest {
+
+    Desert desert;
+    MainCourse mainCourse;
+    Lunch lunch;
+
+    @Before
+    public void setUp() {
+        desert = new Desert();
+        mainCourse = new MainCourse();
+        lunch = new Lunch();
+    }
+
+    @Test
+    public void shouldInitializeLunch() {
+        lunch.setMainCourse(mainCourse);
+        lunch.setDesert(desert);
+
+        Assert.assertNotNull(lunch.getMainCourse());
+        Assert.assertNotNull(lunch.getDesert());
+    }
+
 }
